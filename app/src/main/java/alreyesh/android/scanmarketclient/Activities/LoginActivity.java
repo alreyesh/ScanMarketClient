a0d02be2c9510c0e1a6a3ee4ce7422fae604432e
Alreyesh <alfreyh@gmail.com>
2022-03-13 07:34:41 -0500

    Actualizacion 13-03-2022

--- app/src/main/java/alreyesh/android/scanmarketclient/Activities/LoginActivity.java
+++ app/src/main/java/alreyesh/android/scanmarketclient/Activities/LoginActivity.java
@@ -1,18 +1,28 @@
 package alreyesh.android.scanmarketclient.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import alreyesh.android.scanmarketclient.R;
 import alreyesh.android.scanmarketclient.Utils.Util;
@@ -22,15 +32,37 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTextEmail;
     private EditText editTextPassword;
     private Switch switchRemember;
+    private TextView textRegister;
+    private TextView textReset;
     private Button btnLogin;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         bindUI();
         prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
-
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
         setCredentialsIfExists();
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        textReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ResetActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,8 +70,11 @@ public class LoginActivity extends AppCompatActivity {
                 String password = editTextPassword.getText().toString();
 
                 if(login(email,password)){
-                    goToMain();
-                    saveOnPreferences(email,password);
+                    mDialog.setMessage("Iniciando Sesión");
+                    mDialog.setCanceledOnTouchOutside(false);
+                    mDialog.show();
+                    Auth(email,password);
+
                 }
             }
         });
@@ -48,16 +83,30 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
     private void bindUI(){
-        editTextEmail = (EditText) findViewById(R.id.editTextTextEmail);
+        editTextEmail = (EditText) findViewById(R.id.editTextRecuperarEmail);
         editTextPassword=(EditText)findViewById(R.id.editTextPassword);
         switchRemember = (Switch)findViewById(R.id.switchRemember);
         btnLogin=(Button) findViewById(R.id.buttonLogin);
+        textRegister=(TextView) findViewById(R.id.textRegistrar);
+        textReset = (TextView) findViewById(R.id.textLost);
     }
 
     private void setCredentialsIfExists(){
-        String email = Util.getUserMailPrefs(prefs);
-        String password = Util.getUserPassPrefs(prefs);
+       String email = Util.getUserMailPrefs(prefs);
+       String password = Util.getUserPassPrefs(prefs);
         if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
             editTextEmail.setText(email);
             editTextPassword.setText(password);
@@ -67,10 +116,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private boolean login(String email,String password){
         if(!isValidEmail(email)){
-            Toast.makeText(this,"Email is not valid ,please try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Ingrese Correo Valido",Toast.LENGTH_SHORT).show();
             return false;
         }else if(!isValidPassword(password)){
-            Toast.makeText(this,"Email is not valid ,please try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Ingrese Contraseña Valida",Toast.LENGTH_SHORT).show();
             return false;
         }else{
             return true;
@@ -98,4 +147,28 @@ public class LoginActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+    private void Auth(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // updateUI(user);
+                            goToMain();
+                            saveOnPreferences(email,password);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                        mDialog.dismiss();
+                    }
+                });
+    }
+
 }
--- app/src/main/java/alreyesh/android/scanmarketclient/Activities/MainActivity.java
+++ app/src/main/java/alreyesh/android/scanmarketclient/Activities/MainActivity.java
@@ -2,24 +2,101 @@ package alreyesh.android.scanmarketclient.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import alreyesh.android.scanmarketclient.Fragments.HomeFragment;
 import alreyesh.android.scanmarketclient.R;
 import alreyesh.android.scanmarketclient.Utils.Util;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences prefs;
+    private FirebaseAuth mAuth;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+
+
+    private TextView txtEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+        drawerLayout= (DrawerLayout)findViewById(R.id.drawer_layout);
+        navigationView=(NavigationView) findViewById(R.id.navview);
+
+        setFragmentByDefault();
         prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        mAuth = FirebaseAuth.getInstance();
+        //txtEmail = (TextView)findViewById(R.id.txtEmail) ;
+        FirebaseUser user = mAuth.getCurrentUser();
+        String email = user.getEmail();
+       // txtEmail.setText("Bienvenido, "+email);
+        //Navegacion
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                boolean fragmentTransaction = false;
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.menu_home:
+                        fragment = new HomeFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.menu_logout:
+                        Util.removeSharedPreferences(prefs);
+                        logOut();
+                        break;
+                }
+                if(fragmentTransaction){
+                    changeFragment(fragment,item);
+                    drawerLayout.closeDrawers();
+                }
+                return true;
+            }
+        });
+
+
+
+
     }
 
     @Override
@@ -31,6 +108,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
             case R.id.menu_logout:
                 logOut();
                 return true;
@@ -44,12 +124,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void logOut(){
+        FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(this,LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+    private void setToolbar(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_sand);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+    private void setFragmentByDefault(){
+        changeFragment(new HomeFragment(),navigationView.getMenu().getItem(0));
+    }
+    private void changeFragment(Fragment fragment, MenuItem item){
+        getSupportFragmentManager()
+                .beginTransaction().replace(R.id.content_frame,fragment)
+                .commit();
+        item.setChecked(true);
+        getSupportActionBar().setTitle(item.getTitle());
 
-
+    }
 
 }
