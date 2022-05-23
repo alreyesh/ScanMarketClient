d0d259cfcec765a1f213556b96158012a894ef3b
Alreyesh <alfreyh@gmail.com>
2022-05-23 05:43:55 -0500

    Actualizar HomeFragment: Al darle clic Escanear Producto se abre la camara.

--- app/src/main/java/alreyesh/android/scanmarketclient/Fragments/HomeFragment.java
+++ app/src/main/java/alreyesh/android/scanmarketclient/Fragments/HomeFragment.java
@@ -1,19 +1,25 @@
 package alreyesh.android.scanmarketclient.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import alreyesh.android.scanmarketclient.Activities.LoginActivity;
+import alreyesh.android.scanmarketclient.Activities.MainActivity;
+import alreyesh.android.scanmarketclient.Camara.CamaraActivity;
 import alreyesh.android.scanmarketclient.R;
 
 
 public class HomeFragment extends Fragment {
 
-
+    private Button btnCamera;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,12 +30,24 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-    }
+         }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =inflater.inflate(R.layout.fragment_home, container, false);
+        btnCamera=(Button) view.findViewById(R.id.btnScan);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CamaraActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        return view ;
     }
 }
